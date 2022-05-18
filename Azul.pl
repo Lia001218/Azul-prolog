f9cb66172992fa304c09e79d8e6b37687b8e6690
Lia <liazerqueraf@gmail.com>
2022-05-18 21:42:55 +0000

    rondas

--- Azul.pl
+++ Azul.pl
@@ -8,7 +8,7 @@ create_players(Count_player, Players):-
     findall(P,(
         member(X,List),
         select_strategy(Strategy),
-        assing_strategy(strategy,X,Strategy,P)
+        assing(strategy,X,Strategy,P)
     ),Players_created),
     enumerate(Players_created,1,Players).
 % Se crea la zona de preparacion , primero creamos una lista en la cual vamos a anadir en cada pocicion una lista de la siguiente manera
@@ -21,7 +21,40 @@ create_preparation_zone(Count_player,Preparation_zone):-
     enumerate(List,1,Enum),
     colors(C),
     findall(P,[New:zone, C : valid,C:all]:SZ,
-        (assing_size(SZ,Enum,_),
+        (member_dict(SZ,Enum,_),
         add([],SZ,empty,New))),
     Preparation_zone. 
 
+strategies([basic,greedy]).
+
+select_strategy(S):-
+    strategies(Strategy),
+    random_permutation(Strategy,[S|_]).
+ejecute_round(OldRound,[],NewRound,[]).
+ejecute_round(OldRound,[PlayerActual:Id|Players],NewRound,[Id:FacId|Accions]):-
+    % Vemos cual es la estrategia del jugador actual
+    member_dict(strategy,PlayerActual,St),
+    % decimos quien juega ahora
+    info_log(["Player" ,Id,"empieza el turno"]),
+    %  Se ejecuta la estrategia del jugador actual
+    Play =..[St,Game,PlayerActual,ActualRound,NewPlayer,LineId:FacId:Color],
+    Play,
+    info_log([
+        "Player seleciono",
+        Color ,
+        "de",
+        FacId ,
+        "y lo coloca en linea",
+        LineId]),
+    % ve el estado de las factorias en la ronda actual
+    member_dict(factories,ActualRound,Facs),
+    debug_log([Facs:factories]),
+    info_log([
+        NewPreparation_zone]),
+    % ve el tablero de los jugadores antes de ejecutar la jugada
+    member_dict(players,ActualRound,OldBoard),
+    % Actualiza el tablero
+    assing(Id,OldBoard,NewPreparation_zone,ActualBoard),
+    assing(players,ActualRound,ActualBoard,NextRound),
+    % manda a jugar al proximo jugador
+    ejecute_round(NextRound,Players,NewRound,Accions).
